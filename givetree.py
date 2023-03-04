bf40ab45608af0237d1553e7eb3701fe0c741926
Saksham Yadav <sakshamyadav11@gmail.com>
2023-03-04 15:49:05 +1100

    Update givetree.py

--- givetree.py
+++ givetree.py
@@ -1,22 +1,50 @@
-import streamlit as st
 import openai
+import streamlit as st
 
-def generate_text(prompt, api_key):
+# Define function to extract information from text
+def extract_information(input_text, information, api_key):
     openai.api_key = api_key
-    model_engine = "text-davinci-002"
-    completions = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=2048)
-    message = completions.choices[0].text
-    return message.strip()
+    prompt = (f"Extract the {information} from the following text:\n\n"
+              f"{input_text}\n\n"
+              f"The {information} is:")
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=200,
+        n=1,
+        stop=None,
+        timeout=15,
+    )
+    return response.choices[0].text.strip()
 
-st.title("Celebrity Outfit Finder")
+# Define Streamlit app
+def app():
+    # Set app title
+    st.title("What Did They Wear?")
 
-api_key = st.text_input("Enter your OpenAI API key:")
-celebrity_name = st.text_input("Enter the celebrity name:")
-event_name = st.text_input("Enter the event name:")
+    # Set up input form
+    api_key = st.text_input("Enter your OpenAI API key:")
+    celebrity_name = st.text_input("Enter the name of the celebrity:")
+    event_name = st.text_input("Enter the name of the event:")
+    input_text = st.text_input("Enter the text to extract information from:")
+    information = st.selectbox("Select the information to extract:", ["Dress", "Shoes", "Jewelry", "All"])
 
-if st.button("Find Outfit"):
-    prompt = f"What did {celebrity_name} wear to {event_name}?"
-    result = generate_text(prompt, api_key)
-    prompt = f"extract key information from: {result}"
-    entities = generate_text(prompt, api_key)
-    st.write(result)
+    # Extract information on button click
+    if st.button("Extract Information"):
+        if not api_key:
+            st.warning("Please enter your OpenAI API key.")
+        elif not celebrity_name:
+            st.warning("Please enter the name of the celebrity.")
+        elif not event_name:
+            st.warning("Please enter the name of the event.")
+        elif not input_text:
+            st.warning("Please enter some text to extract information from.")
+        elif information == "All":
+            dress = extract_information(input_text, "dress", api_key)
+            shoes = extract_information(input_text, "shoes", api_key)
+            jewelry = extract_information(input_text, "jewelry", api_key)
+            st.write(f"{celebrity_name} wore {dress}, paired with {shoes} and {jewelry} at {event_name}.")
+        else:
+            extracted_info = extract_information(input_text, information.lower(), api_key)
+            st.write(f"{celebrity_name} wore {extracted_info} at {event_name}.")
