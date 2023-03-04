b489c9aaf90da1683439e4bfae5a702946e17e0d
Saksham Yadav <sakshamyadav11@gmail.com>
2023-03-04 14:56:15 +1100

    Update givetree.py

--- givetree.py
+++ givetree.py
@@ -21,7 +21,7 @@ if st.button("Generate Text Art"):
             "Authorization": f"Bearer {api_key}" # Use the API key entered by the user
         },
         json={
-            "model": "image-alpha-001", # CLIP guided Diffusion model to use
+            "model": "image-alpha-003", # CLIP guided Diffusion model to use
             "prompt": f"Generate an image of '{text}'",
             "num_images": num_images,
             "size": "1024x1024",
