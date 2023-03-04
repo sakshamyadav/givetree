1f9687921832e6cff6fa3e685229367829129055
Saksham Yadav <sakshamyadav11@gmail.com>
2023-03-04 15:35:08 +1100

    Update givetree.py

--- givetree.py
+++ givetree.py
@@ -4,19 +4,9 @@ import openai
 def generate_text(prompt, api_key):
     openai.api_key = api_key
     model_engine = "text-davinci-002"
-    completions = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=1024)
+    completions = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=2048)
     message = completions.choices[0].text
     return message.strip()
-
-def extract_keywords(text):
-    openai.api_key = api_key
-    model_engine = "text-davinci-002"
-    completions = openai.Completion.create(engine=model_engine, prompt=text, max_tokens=1024,
-                                            model="text-davinci-002", 
-                                            model_output_prefix="ENTITIES:\n")
-    entities_text = completions.choices[0].text
-    entities = entities_text.split("\n")[1:-1]
-    return entities
 
 st.title("Celebrity Outfit Finder")
 
@@ -28,11 +18,3 @@ if st.button("Find Outfit"):
     prompt = f"What did {celebrity_name} wear to {event_name}?"
     result = generate_text(prompt, api_key)
     st.write(result)
-
-    keywords = extract_keywords(result)
-    if len(keywords) > 0:
-        st.write("Keywords found in the text:")
-        for keyword in keywords:
-            st.write(f"- {keyword}")
-    else:
-        st.write("No keywords found in the text.")
